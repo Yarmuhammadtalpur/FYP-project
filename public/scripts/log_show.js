e5c07034321cf3f5c367b189cdc1edca16f54ac0
yar muhammad <miryarmuhammadtalpur@gmail.com>
2021-10-11 01:03:46 +0500

    Auth System DOne

--- public/scripts/log_show.js
+++ public/scripts/log_show.js
@@ -1,6 +1,7 @@
 var pwd = document.getElementById('Password');
 var eye = document.getElementById('flexSwitchCheckDefault');
 var show3 = document.getElementById('show3');
+var btn_submit = document.getElementById('btn_submit');
 
 eye.addEventListener('click',togglePass);
 
@@ -10,4 +11,13 @@ function togglePass(){
 
    (pwd.type == 'password') ? pwd.type = 'text' : pwd.type = 'password';
    (show3.innerHTML == 'Show Password') ? show3.innerHTML = 'Hide Password' : show3.innerHTML = 'Show Password';
+}
+
+
+btn_submit.addEventListener('click',togglePass2);
+
+function togglePass2(){
+
+   pwd.type = 'password';
+
 }
